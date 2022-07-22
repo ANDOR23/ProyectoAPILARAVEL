2bc3c07ff19c35ac215ad347f66a762b5b67dd00
ANDOR23 <pirlo199994@gmail.com>
2022-07-22 15:19:55 -0500

    second commit...

---
+++ app/Http/Controllers/Api/v1/AuthController.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Http\Controllers\Api\v1;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class AuthController extends Controller
+{
+    //CREAR UNA INSTANCIA DE AuthController
+
+    public function __construct(){
+        $this->middleware('auth:api', ['except' => ['login']]);
+    }
+
+    public function login(){
+        $credentials = request(['email', 'password']);
+        if(! $token = auth()->attempt($credentials)){
+            return response ()->json(['error' => 'Unauthorized'], 401);
+        }
+        return $this->respondWithToken($token);
+    }
+
+    public function me(){
+        return response()->json(auth()->user());
+    }
+
+    public function logout(){
+        auth()->logout();
+        return response()->json(['message' => 'Successfily logged out']);
+    }
+
+    public function refresh(){
+        return $this->respondWithToken(auth()->refresh());
+    }
+
+    public function respondWithToken($token){
+        return response()->json([
+            'access_token' =>$token,
+            'token_type' => 'bearer',
+            'expires_in' => auth()->factory()->getTTL() * 60
+        ]);
+    }
+}
---
+++ routes/api.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
+|
+*/
+/*
+Route::middleware('auth:api')->get('/user', function (Request $request) {
+    return $request->user();
+});
+*/
+Route::apiResource('v1/posts', App\Http\Controllers\Api\v1\PostController::class)->middleware('api');
+
+Route::group([
+    'middleware' => 'api',
+    'prefix' => 'v1/auth'],
+
+    function ($router){
+        //Route::post('login', 'AuthController@login');
+        Route::post('login', [\App\Http\Controllers\Api\v1\AuthController::class, 'login'])-> name('login');
+        //Route::post('logout', 'AuthController@logout');
+        Route::post('logout', [\App\Http\Controllers\Api\v1\AuthController::class, 'logout'])-> name('logout');
+        //Route::post('refresh', 'AuthController@refresh');
+        Route::post('refresh', [\App\Http\Controllers\Api\v1\AuthController::class, 'refresh'])-> name('refresh');
+        //Route::post('me', 'AuthController@me');
+        Route::post('me', [\App\Http\Controllers\Api\v1\AuthController::class, 'me'])-> name('me');
+        Route::post('register', 'App\Http\Controllers\Api\v1\RegisterController@store');
+    }
+);
